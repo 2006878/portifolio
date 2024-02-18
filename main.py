cc32c03f5105dba06aed69e4d292c3bb9f085b4d
Tairone <taironeamaral@bol.com.br>
2024-02-18 10:08:10 -0300

    melhoria no layout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from st_pages import Page, show_pages
+import base64
 
 # Carreguando o ícone da aba
 favicon = "img/dados.png"
@@ -32,6 +33,7 @@ st.write("Desenvolvimento de software")
 st.write("Gerenciamento de Equipe")
 st.write("Ciência de Dados")
 st.write("Inovação em Negócios")
+st.markdown("---")
 
 # Experiências
 st.subheader("Experiências")
@@ -41,16 +43,33 @@ st.write("GetinOxy (2022 - 2023): Líder técnico do time back-end e infraestrut
 st.write("Serralheria Amaral (Setembro de 2022 - Atual): Diretor Geral, com resultado de aumento de 55% no faturamento anual.")
 st.write("Zayon Data Mine (Junho de 2023 - Atual): Cientista de Dados, liderando projetos de pesquisa e desenvolvimento de modelos de Inteligência Artificial. Construção de plataforma integrando ferramentas e análises para otimização de decisões.")
 st.write("TaaS (2023 - Atual): 'Talent as a Service', fornecendo suporte estratégico e consultivo a Startups.")
+st.markdown("---")
 
 # Hobbies e Objetivos
 st.subheader("Hobbies e Objetivos")
 st.write("Hobbies: Leitura, prática de Jiu-Jitsu (instrutor) e viagens com a família.")
 st.write("Objetivos: Impactar cada vez mais pessoas, colaborando na construção e manutenção de produtos e projetos que impactem positivamente a vida delas.")
+st.markdown("---")
 
 # Contatos
 st.subheader("Contatos")
-st.write("Telefone: 35 992579211")
-st.write("LinkedIn: [Perfil do LinkedIn de Tairone Amaral](https://www.linkedin.com/in/tairone-amaral/)")
-
+link_url = "https://www.linkedin.com/in/tairone-amaral/"
+link_wpp = "https://api.whatsapp.com/send?phone=5535992579211"
 qr_linkedin = "img/QRLinkedin.png"
-st.image(qr_linkedin, use_column_width=False, width=150)
+qr_whatsapp = "img/logo_wpp.png"
+
+# Adiciona os QRCode dos contatos
+
+def get_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_linkedin = get_base64(qr_linkedin)
+image_wpp = get_base64(qr_whatsapp)
+
+col1, col2 = st.columns(2)
+
+col1.markdown(f'<a href="{link_url}"><img src="data:image/png;base64,{image_linkedin}" alt="linkedin" style="width: 100px; height: 100px;"/></a>', unsafe_allow_html=True)
+col1.caption("Linkedin")
+col2.markdown(f'<a href="{link_wpp}"><img src="data:image/png;base64,{image_wpp}" alt="linkedin" style="width: 100px; height: 100px;"/></a>', unsafe_allow_html=True)
+col2.caption("Whatsapp")
